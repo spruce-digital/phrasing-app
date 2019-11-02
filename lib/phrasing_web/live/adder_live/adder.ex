768ef510a35bab5e2b32648704b0cdd0f475437c
Ben Yorke <bendyorke@Bens-MacBook-Air.local>
2019-11-02 12:39:46 +0100

    Updated adder

--- lib/phrasing_web/live/adder_live/adder.ex
+++ lib/phrasing_web/live/adder_live/adder.ex
@@ -4,12 +4,20 @@ defmodule PhrasingWeb.AdderLive.Adder do
   alias Phrasing.Dict
   alias Phrasing.Dict.Phrase
   alias PhrasingWeb.AdderView
+  alias Ecto.Changeset
+
+  def new_changeset() do
+    Dict.change_phrase(%Phrase{lang: "nl"})
+  end
 
   def mount(_session, socket) do
-    changeset = Dict.change_phrase(%Phrase{lang: "nl"})
+    changeset = new_changeset
     interpretation = :english
+    languages = Phrase.languages
 
-    {:ok, assign(socket, open: true, changeset: changeset, interpretation: interpretation)}
+    {:ok, assign(socket, open: true, changeset: changeset,
+      interpretation: interpretation, select_language: false,
+      languages: languages)}
   end
 
   def render(assigns) do
@@ -20,7 +28,36 @@ defmodule PhrasingWeb.AdderLive.Adder do
     {:noreply, assign(socket, open: true)}
   end
 
+  def handle_event("close", _params, socket) do
+    {:noreply, assign(socket, open: false)}
+  end
+
+  def handle_event("select_language", %{"lang" => lang}, socket) do
+    changeset = Changeset.put_change socket.assigns.changeset, :lang, lang
+    {:noreply, assign(socket, select_language: false, changeset: changeset)}
+  end
+  def handle_event("select_language", _params, socket) do
+    {:noreply, assign(socket, select_language: true)}
+  end
+
   def handle_event("change_interpretation", %{"interpretation" => interp}, socket) do
     {:noreply, assign(socket, interpretation: String.to_atom(interp))}
+  end
+
+  def handle_event("update", %{"_target" => target, "phrase" => phrase}, socket) do
+    value = get_in phrase, tl(target)
+    field = target
+            |> List.last
+            |> String.to_atom
+
+    changeset = Changeset.put_change socket.assigns.changeset, field, value
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("submit", %{"phrase" => phrase}, socket) do
+    IO.inspect phrase
+
+    {:noreply, assign(socket, open: false, changeset: new_changeset)}
   end
 end
--- lib/phrasing_web/views/adder_view.ex
+++ lib/phrasing_web/views/adder_view.ex
@@ -1,10 +1,27 @@
 defmodule PhrasingWeb.AdderView do
   use PhrasingWeb, :view
+
   alias Phrasing.Dict.Phrase
+  alias Ecto.Changeset
 
   def language_name language_code do
     Phrase.languages
     |> Enum.find(fn x -> x[:value] == language_code end)
     |> Access.get(:key)
+  end
+
+  def language_class language, changeset do
+    value = Access.get(language, :value)
+    current = Changeset.get_field(changeset, :lang)
+
+    if value == current do
+      "language language-selected"
+    else
+      "language"
+    end
+  end
+
+  def interpretations do
+    [:english, :literal, :translit]
   end
 end
