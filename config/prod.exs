a9a271d3396aa2769c474e77a23e3448cabbf2d0
Ben Yorke <bendyorke@arya.local>
2019-12-02 12:29:45 +0100

    Remove gigalixir config

--- config/prod.exs
+++ config/prod.exs
@@ -75,11 +75,11 @@ config :phoenix, :serve_endpoints, true
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
+
+# config :phrasing, Phrasing.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   url: System.get_env("DATABASE_URL"),
+#   ssl: true,
+#   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
 import_config "prod.secret.exs"
-
-config :phrasing, Phrasing.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-
