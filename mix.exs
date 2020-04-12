13ad3a67d35d4d941b1b303820f6ca09f4044140
Ben Yorke <bendyorke@arya.local>
2020-04-12 17:59:47 +0200

    v1.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Phrasing.MixProject do
   def project do
     [
       app: :phrasing,
-      version: "1.1.4",
+      version: "1.1.5",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
